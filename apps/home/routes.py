cb2f44301c2cbbf19bb89d0a5e4ece8698c45146
VaibhaviRautCG <vaibhavi.raut@capgemini.com>
2023-12-12 10:48:58 +0530

    Template page for Chatbot.

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template
+import traceback
 
 from apps.home import blueprint
 
@@ -6,3 +7,12 @@ from apps.home import blueprint
 def index():
     # return "Hello, Flask! This is the first dummy page."
     return render_template('home/index.html')
+
+
+@blueprint.route('/chatbot',methods=['GET', 'POST'])
+def chatbot():
+    try:
+        return render_template('home/chatbot.html', segment='chatbot')
+    except Exception as err:
+        print("Exception or Error at Chatbot page: ",err)
+        traceback.print_exc()
