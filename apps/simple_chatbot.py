a1c39201b36087e8be0c785ea51512a444db0f6a
Suryawanshi <sakshi.suryawanshi@capgemini.com>
2023-12-15 20:33:47 +0530

    updated code

--- apps/simple_chatbot.py
+++ apps/simple_chatbot.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-from .simple_chatbot import get_response  # Import your chatbot function
+#from .simple_chatbot import get_response  # Import your chatbot function
 
 
 app = Flask(__name__)
